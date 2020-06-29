420de53a7ea7e1e0e26827f0e79608877e4e4709
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-29 18:34:32 -0400

    Add haloquote.js command

---
+++ commands/haloquote.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+module.exports = {
+    name: 'haloquote',
+    description: 'Send a line of dialogue from the Halo series! Quotes are picked at random.',
+    cooldown: 0,
+    execute(message, args) {
+        //Reads each line from text file that supplies lines of dialogue. Picks on at random and displays it.
+        var fs = require('fs');
+        var array = fs.readFileSync('halolines.txt').toString().split("\n");
+        var quote = Math.floor(Math.random() * array.length);
+        message.channel.send(array[quote]);
+    },
+};
