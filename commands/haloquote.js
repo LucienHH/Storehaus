c6dac45ec82718864346e96ae3c32da13c00b30d
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-06-29 18:01:38 -0400

    Add haloquote.js command

---
+++ commands/haloquote.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+module.exports = {
+    name: 'haloquote',
+    description: 'Send a line of dialogue from the Halo series! Quotes are picked at random.',
+    cooldown: 0,
+    execute(message, args) {
+        var quoteList = ["Halo Lines Go Here!"];
+		var quote = Math.floor(Math.random() * quoteList.length);
+		message.channel.send(quoteList[quote]);
+        //message.channel.send("Kappa bot best bot, mean.. PONG!!")
+    },
+};
