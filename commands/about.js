977eb7cb8ef15f377eb8f4ceffb1ca19cb5f180a
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-13 13:15:40 -0400

    Add reddit.js

--- commands/about.js
+++ commands/about.js
@@ -6,6 +6,12 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 
-        message.channel.send("This bot is made to show information about games! Made by AndyTheNerd and PatrossDev. Contribute to this project at https://github.com/AndyTheNerd/GameInfoBot/")
-	},
+		//message.channel.send("This bot is made to show information about games! Made by AndyTheNerd and PatrossDev. Contribute to this project at https://github.com/AndyTheNerd/GameInfoBot/")
+		let embed = new Discord.MessageEmbed()
+		.setColor("#ff00ff")
+		.addField(`Storehaus: A Gaming Info Bot`, " [Add Storehaus to your server!](https://discordapp.com/oauth2/authorize?client_id=725749175282696194&scope=bot&permissions=8) Storehaus is a gaming info bot designed to help you learn and enhance your gaming experience. Storehaus can grab game reviews, information, sales, playtime, and more! This bot is open source, so you can check out the code of Storehaus and contribute to it [here](https://github.com/AndyTheNerd).")
+		.setFooter("Made with <3 from AndyTheNerd and PatrossDev.");
+		message.channel.send(embed);
+		delete embed;
+	}
 };
---
+++ commands/reddit.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Fetch required for API Call
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'xbl',
+    description: 'Get deals about a game on steam. Try !gogdeals `name of game`!',
+    cooldown: 5,
+    async execute(message, args) {
+        //GET Game deals by title name
+        fetch(`https://www.reddit.com/r/nocontextxboxmessages.json?show=all&limit=1`)
+            .then(response => response.json())
+            .then(data => data.map( d => {
+                console.log(d)
+                //Store for converting sale date from epoch time
+                const embed = new Discord.MessageEmbed()
+                .setColor("#ff00ff")
+                .setTitle(`No Context XBL Message`)
+                .addField(`Message` , `$${d.url}`)
+                message.channel.send(embed);
+
+                delete embed;
+                
+            }));
+        return;
+    }
+};
