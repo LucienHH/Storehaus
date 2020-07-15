5c1a29a36dac72180e22cda36dd71bdf4b47183e
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-15 18:00:34 -0400

    Enhance about information

--- commands/about.js
+++ commands/about.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 module.exports = {
 	name: 'about',
-	description: 'Game Info Bot is a bot that queries various APIs to show the most helpful gaming information. This bot was created to help gamers discover, share, and enjoy info on their favorite games!',
+	description: 'Game Info Bot is a bot that queries various APIs to show the most helpful gaming information. This bot was created to help gamers discover, share, and enjoy info on their favorite games! To run, type `!about',
 	cooldown: 5,
 	execute(message, args) {
 
@@ -10,7 +10,12 @@ module.exports = {
 		let embed = new Discord.MessageEmbed()
 		.setColor("#ff00ff")
 		.addField(`Storehaus: A Gaming Info Bot`, " [Add Storehaus to your server!](https://discordapp.com/oauth2/authorize?client_id=725749175282696194&scope=bot&permissions=8) Storehaus is a gaming info bot designed to help you learn and enhance your gaming experience. Storehaus can grab game reviews, information, sales, playtime, and more! This bot is open source, so you can check out the code of Storehaus and contribute to it [here](https://github.com/AndyTheNerd).")
-		.setFooter("Storehaus v1.0.0. Made with <3 from AndyTheNerd and PatrossDev.");
+		.addField(`Gaming Commands`,"`!compat`, `!gameinfo`, `!gogdeals`, `!goty`, `!halopedia`, `!haloquote`, `!playtime`, `!review`, `!steamdeals`, `!xbl`")
+		.addField(`Humor and Info Commands`, "`!dadjoke`, `!insult`, `!trivia`")
+		.addField(`Support Comands`, "`!help [name of a command]`, `!bugreport`")
+		.addField(`TeamRespawn`, "If you love Halo or Halo Wars, come join the [TeamRespawn Discord server!](https://discord.com/invite/Q5vEpDj)")
+		.addField(`Support`, "Need help? Join the [support server.](https://discord.gg/nvNQQ2f)")
+		.setFooter("Storehaus v 1.0.0 Made with <3 from AndyTheNerd and PatrossDev.");
 		message.channel.send(embed);
 		delete embed;
 	}
