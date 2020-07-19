030795f05f882c3bc9b3ae5b32198fa71e47390c
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-19 09:21:49 -0400

    Add meme.js command

---
+++ commands/meme.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Fetch required for API Call
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'meme',
+    description: 'Get some of the best gaming memes around. Content may contain vulgarity. To run, type `!meme`',
+    cooldown: 5,
+    async execute(message, args) {
+        var array = [`https://www.reddit.com/r/halomemes/random.json?show=all&limit=1`, `https://www.reddit.com/r/skyrimmemes/random.json?show=all&limit=1`, `https://www.reddit.com/r/pcmasterrace/random.json?show=all&limit=1`, `https://www.reddit.com/r/MinecraftMemes/random.json?show=all&limit=1`, `https://www.reddit.com/r/nocontextxboxmessages/random.json?show=all&limit=1`];
+        var subreddit = array[Math.floor(Math.random() * array.length)];
+        //Get a random post from subreddit
+        fetch(subreddit)
+            .then(response => response.json())
+            .then(data => {
+                var imageLink = data[0].data.children.map(d => d.data.url_overridden_by_dest);
+                var postTitle = data[0].data.children.map(d => d.data.title);
+                imageLink = imageLink.toString(); // converts from object
+                const embed = new Discord.MessageEmbed()
+                .setColor("#ff00ff")
+                .addField(`${postTitle}` , `${imageLink}`)
+                .setImage(imageLink)
+                message.channel.send(embed);                
+            });
+            delete embed;
+        return;
+    }
+};
