3d9934e56f37c3a2e6ef4074e4bb09fa00d0591c
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-19 09:20:58 -0400

    Add newest commands info

--- commands/about.js
+++ commands/about.js
@@ -11,10 +11,10 @@ module.exports = {
 		.setColor("#ff00ff")
 		.addField(`Storehaus: A Gaming Info Bot`, " [Add Storehaus to your server!](https://discordapp.com/oauth2/authorize?client_id=725749175282696194&scope=bot&permissions=8) Storehaus is a gaming info bot designed to help you learn and enhance your gaming experience. Storehaus can grab game reviews, information, sales, playtime, and more! This bot is open source, so you can check out the code of Storehaus and contribute to it [here](https://github.com/AndyTheNerd).")
 		.addField(`Gaming Commands`,"`!compat`, `!gameinfo`, `!gogdeals`, `!goty`, `!halopedia`, `!haloquote`, `!playtime`, `!review`, `!steamdeals`, `!xbl`")
-		.addField(`Humor and Info Commands`, "`!dadjoke`, `!insult`, `!trivia`")
+		.addField(`Humor and Info Commands`, "`!chuck`, `!dadjoke`, `!insult`, `!meme`, `!trivia`")
 		.addField(`Support Comands`, "`!help [name of a command]`, `!bugreport`")
 		.addField(`TeamRespawn`, "If you love Halo or Halo Wars, come join the [TeamRespawn Discord server!](https://discord.com/invite/Q5vEpDj)")
-		.addField(`Support`, "Need help? Join the [support server.](https://discord.gg/nvNQQ2f)")
+		.addField(`Support`, "Need help? Join the [support server.](https://discord.gg/nvNQQ2f) Feeling generous? [Buy us a coffee.](https://ko-fi.com/andythenerd)")
 		.setFooter("Storehaus v 1.0.0 Made with <3 from AndyTheNerd and PatrossDev.");
 		message.channel.send(embed);
 		delete embed;
