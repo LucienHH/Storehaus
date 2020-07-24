a77de5d8b63388f2d36cda54457d590065e95867
PatrossDev <contactpatross@gmail.com>
2020-07-24 02:34:59 +0100

    weather data grabbing implemented

--- commands/weather.js
+++ commands/weather.js
@@ -12,17 +12,26 @@ module.exports = {
     async execute(message, args) {
         let option = args.slice(0).join(" ");
 
-        fetch(`api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=${process.env.open_weather}`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${args[0]}&appid=${process.env.open_weather}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data.query.map(d => {
-                console.log(d)
+            // console.log(data.query.map(d => {
+            //     console.log(d)
                 //modify title string so URL works
                 let embed = new Discord.MessageEmbed()
                 .setColor("#ff00ff")
-                .addField(`Weather for ${args[0]}` ,d)
+                .setTitle(`Weather for ${args[0]}`)
+                .addField("Temp:",data.main.temp)
+                .addField("Feels like:",data.main.feels_like)
+                .addField("Min temp:",data.main.temp_min)
+                .addField("Max temp:",data.main.temp_max)
+                .addField("Humidity:",data.main.humidity)
+                .addField("Sunrise:",data.sys.sunrise)
+                .addField("Sunset",data.sys.sunset)
                 message.channel.send(embed);
-            }))
+            // }))
+
+            // console.log(data);
         }
         );
         delete embed;
