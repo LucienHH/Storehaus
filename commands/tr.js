17a0436736a2c066ac4f3cd7711be64b5b030391
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-26 14:46:22 -0400

    Somehow this doesn't work

---
+++ commands/tr.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+module.exports = {
+	name: 'teamrespawn',
+	description: 'Learn more about the TeamRespawn community. To run, type `!teamrespawn',
+	cooldown: 5,
+	execute(message, args) {
+		let embed = new Discord.MessageEmbed()
+		.setColor("#ff00ff")
+		.addField(`TeamRespawn Community`, "TeamRespawn is a YouTube channel and Discord that focuses on a friendly and welcoming environment in the Halo and Halo Wars games. If you love Halo, you'll love TeamRespawn. Come join the [TeamRespawn Discord server!](https://discord.com/invite/Q5vEpDj) and subscribe to the [YouTube channel](https://www.youtube.com/teamrespawntv)")
+        .setImage("../images/TeamRespawn.jpg")
+		message.channel.send(embed);
+		delete embed;
+	}
+};	
