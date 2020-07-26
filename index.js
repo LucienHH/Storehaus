77f6e7cc4debc2d893508c7b5b91fb31fb394138
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-26 11:10:00 -0400

    Bring back index.js oops

---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config({ path: require('find-config')('.env') });
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//This is for REST API.
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
+client.commands = new Discord.Collection();
+
+/// GO THROUGH EVERY FILE IN COMMANDS FOLDER AND GRAB ALL JS FILES
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+client.once('ready', () => {
+	console.log('Ready to go!');
+
+	client.user.setActivity(`!about in ${client.guilds.cache.size} servers`, {
+		type: "STREAMING",
+		//url: "Some URL here"
+	});
+});
+
+client.on('message', async message => {
+	if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(process.env.prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(c => c.aliases && c.aliases.includes(commandName));
+
+	if (!command) return;
+
+	//Implement Cooldowns, prevent spam
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`).then(m => m.delete({timeout: 5000}));
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	//End Cooldown implementation
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
+});
+
+client.login(process.env.token);
