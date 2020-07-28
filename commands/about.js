601458978be6b040b1c63dd18be6b5be4bc3527d
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-28 11:37:09 -0400

    Temporary change while we setup support server

--- commands/about.js
+++ commands/about.js
@@ -10,13 +10,13 @@ module.exports = {
 		//message.channel.send("This bot is made to show information about games! Made by AndyTheNerd and PatrossDev. Contribute to this project at https://github.com/AndyTheNerd/GameInfoBot/")
 		let embed = new Discord.MessageEmbed()
 		.setColor("#ff00ff")
-		.addField(`Storehaus: A Gaming Info Bot`, " [Invite Storehaus to your server!](https://discordapp.com/oauth2/authorize?client_id=736204347171405904&scope=bot&permissions=8)\n \nStorehaus is a gaming info bot designed to help you learn and enhance your gaming experience. Storehaus can grab game reviews, information, sales, playtime, and more! This bot is open source, so you can check out the code of Storehaus and contribute to it [here](https://github.com/AndyTheNerd).\n \nThe default prefix for Storehaus is `!` and you can change it with `!setprefix`")
+		.addField(`Storehaus: A Gaming Info Bot`, " Invite Storehaus to your server! (Coming Soon)\n \nStorehaus is a gaming info bot designed to help you learn and enhance your gaming experience.\n \nThe default prefix for Storehaus is `!` and you can change it with `!setprefix`")
 		.addField(`Gaming Commands`,"`!achievements`, `!compat`, `!dlc` `!gameinfo`, `!gamesub`, `!gogdeals`, `!goty`, `!halopedia`, `!haloquote`, `!playtime`, `!playanywhere`, `!psnow`, `!review`, `!screenshot`, `!series`, `!steamdeals`, `!stores`, `!xbl`")
 		.addField(`Humor and Misc. Commands`, "`!cat`, `!chuck`, `!dadjoke`, `!dog`, `!gif`, `!insult`, `!mars`, `!meme`, `!nasa`, `!teamrespawn`, `!trivia`, `!weather`")
 		.addField(`Support Comands`, "`!about`, `!help`, `!invite`, `!feedback`")
 		.addField(`Help`, "To learn how to use a particular command, type `!help [name of a command]`")
 		.addField(`TeamRespawn`, "If you love Halo or Halo Wars, come join the [TeamRespawn Discord server!](https://discord.com/invite/Q5vEpDj)")
-		.addField(`Support`, "Need help? Join the [support server.](https://discord.gg/nvNQQ2f) Feeling generous? [Buy us a coffee.](https://ko-fi.com/andythenerd)")
+		.addField(`Support`, "Need help? Join the support server (Coming Soon) Feeling generous? [Buy us a coffee.](https://ko-fi.com/andythenerd)")
 		.setFooter(helpers.getFooter());
 		message.channel.send(embed);
 		delete embed;
