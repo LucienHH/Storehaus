cd1208b21fcfd3b457c0db7cb024c3928483886b
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-07-31 21:22:48 -0400

    WIP commit

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -8,7 +8,7 @@ const helpers = require('../helpers/helpers');
 module.exports = {
     name: 'pokemon',
     description: 'Retrieve the NASA picture of the day! To run, try `!nasa`',
-    cooldown: 60,
+    cooldown: 5,
     async execute(message, args) {
         let pokemon = args.slice(0).join(" ");
 
@@ -19,14 +19,16 @@ module.exports = {
             let title = new Discord.MessageEmbed() 
             title.setColor("#ff00ff")
             title.setTitle(`Pokemon Info for ${pokemon}`)
-
+            title.setDescription(`**Versions Appeared In:**`)
             let i=0;
             data.game_indices.map(d => {
                 title.addField(`\u200b` ,`**${++i}** - *${d.version.name}*`, true);
             })
-                //ddField(`Description`, `${trimmedString}...\n Author: ${author}. Date Taken: ${dateTaken}`)
-                title.setFooter(helpers.getFooter());
-                message.channel.send(title);
+            data.types.map(d => {
+            title.addField(`Type:`,`${d.type.name}`)
+            })
+            title.setFooter(helpers.getFooter());
+            message.channel.send(title);
         }
         );
         delete embed;
