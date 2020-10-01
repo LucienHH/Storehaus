4284b7978bb54c901f468e2f3ac10555d1eeb7d2
AndyTheNerd <30298320+AndyTheNerd@users.noreply.github.com>
2020-10-01 17:56:19 -0400

    Updated permissions

--- commands/invite.js
+++ commands/invite.js
@@ -7,6 +7,6 @@ module.exports = {
     usage: ` `,
     execute(message, args) {
 
-        message.channel.send("https://discord.com/oauth2/authorize?client_id=736204347171405904&scope=bot&permissions=379968")
+        message.channel.send("https://discord.com/oauth2/authorize?client_id=736204347171405904&scope=bot&permissions=388288")
     },
 };
