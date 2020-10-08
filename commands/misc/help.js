4503f492c8c9e4eeaa23340d6f41742892d8ad08
PatrossDev <contactpatross@gmail.com>
2020-10-08 16:50:16 +0100

    fixed help issue

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -2,16 +2,16 @@ require('dotenv').config({ path: require('find-config')('.env') });
 const Discord = require('discord.js');
 //required
 const fs = require('fs');
-const prefix  = process.env.prefix;
+const prefix = process.env.prefix;
 const helpers = require('../../helpers/helpers');
 module.exports = {
     name: 'help',
     category: "misc",
     aliases: [],
-	description: 'List info about a specific command.',
+    description: 'List info about a specific command.',
     cooldown: 2,
     usage: '[command name]',
-	execute(message, args, client) {
+    execute(message, args, client) {
         const data = [];
         const { commands } = message.client;
         //// ----------Leaving this commented out in case we use it in the future---------------////
@@ -34,26 +34,25 @@ module.exports = {
         //             message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
         //         });
         // //------------------------------------------------------------------------------//
-        let name = args.slice(0).join(" ");    
+        let name = args.slice(0).join(" ");
 
-        if (name == undefined || name=="") {
+        if (name == undefined || name == "") {
             //message.channel.send(new Discord.MessageEmbed().setTitle("Try !help `name of a command`. To view all commands, type `!about`"));
             let embed = new Discord.MessageEmbed()
-            .setColor("#ff00ff")
-            .setTitle("Storehaus Commands List")
-            .addField("🎮 Gaming Commands", "To see more detailed info, run `!help gaming`")
-            .addField("😂 Fun Commands", "To see more detailed info, run `!help fun`")
-            .addField("⚙️ Misc Commands", "To see more detailed info, run `!help misc`")
+                .setColor("#ff00ff")
+                .setTitle("Storehaus Commands List")
+                .addField("🎮 Gaming Commands", "To see more detailed info, run `!help gaming`")
+                .addField("😂 Fun Commands", "To see more detailed info, run `!help fun`")
+                .addField("⚙️ Misc Commands", "To see more detailed info, run `!help misc`")
             embed.setFooter(helpers.getFooter());
             message.channel.send(embed);
             delete embed;
             return;
         }
-        else if (name.toLowerCase() == "gaming")
-        {
+        else if (name.toLowerCase() == "gaming") {
             let embed = new Discord.MessageEmbed()
-            .setColor("#ff00ff")
-            .setTitle(`Gaming Commands`)
+                .setColor("#ff00ff")
+                .setTitle(`Gaming Commands`)
             const cmd = client.commands;
             let string = "";
             cmd.forEach(element => {
@@ -65,11 +64,10 @@ module.exports = {
             embed.addField(string, `\u200b`);
             message.channel.send(embed)
         }
-        else if (name.toLowerCase() == "fun")
-        {
+        else if (name.toLowerCase() == "fun") {
             let embed = new Discord.MessageEmbed()
-            .setColor("#ff00ff")
-            .setTitle(`Fun Commands`)
+                .setColor("#ff00ff")
+                .setTitle(`Fun Commands`)
             const cmd = client.commands;
             let string = "";
             cmd.forEach(element => {
@@ -81,11 +79,10 @@ module.exports = {
             embed.addField(string, `\u200b`);
             message.channel.send(embed)
         }
-        else if (name.toLowerCase() == "misc")
-        {
+        else if (name.toLowerCase() == "misc") {
             let embed = new Discord.MessageEmbed()
-            .setColor("#ff00ff")
-            .setTitle(`Misc Commands`)
+                .setColor("#ff00ff")
+                .setTitle(`Misc Commands`)
             const cmd = client.commands;
             let string = "";
             cmd.forEach(element => {
@@ -99,33 +96,33 @@ module.exports = {
         }
         else {
             const command = args[0];
-			if (client.commands.has(command)) {
-				cmd = client.commands.get(command);
+            if (client.commands.has(command)) {
+                cmd = client.commands.get(command);
                 const embed = new Discord.MessageEmbed().setColor('00FF00');
-				cmd.name ? embed.setTitle(`Help for **\`${cmd.name}\`**`) : null;
-				cmd.aliases ? embed.addField('Aliases', `${cmd.aliases.join(', ')}`) : null;
-				cmd.description ? embed.addField('Description', `${cmd.description}`) : null;
-				cmd.usage ? embed.addField('Usage', `${prefix}${cmd.name} ${cmd.usage}`) : null;
-				cmd.cooldown ? embed.addField('Cooldown', `${cmd.cooldown} seconds`) : null;
+                cmd.name ? embed.setTitle(`Help for **\`${cmd.name}\`**`) : null;
+                cmd.aliases.length > 0 ? embed.addField('Aliases', `${cmd.aliases.join(', ')}`) : null;
+                cmd.description ? embed.addField('Description', `${cmd.description}`) : null;
+                cmd.usage ? embed.addField('Usage', `${prefix}${cmd.name} ${cmd.usage}`) : null;
+                cmd.cooldown ? embed.addField('Cooldown', `${cmd.cooldown} seconds`) : null;
 
-				message.channel.send(embed);
-				return;
-			}
-			else if (client.aliases.has(command)) {
-				cmd = client.commands.get(client.aliases.get(command));
-				const embed = new Discord.MessageEmbed().setColor('00FF00');
-				cmd.name ? embed.setTitle(`Help for **\`${cmd.name}\`**`) : null;
-				cmd.aliases ? embed.addField('Aliases', `${cmd.aliases.join(', ')}`) : null;
-				cmd.description ? embed.addField('Description', `${cmd.description}`) : null;
-				cmd.usage ? embed.addField('Usage', `${prefix}${cmd.name} ${cmd.usage}`) : null;
-				cmd.cooldown ? embed.addField('Cooldown', `${cmd.cooldown} seconds`) : null;
+                message.channel.send(embed);
+                return;
+            }
+            else if (client.aliases.has(command)) {
+                cmd = client.commands.get(client.aliases.get(command));
+                const embed = new Discord.MessageEmbed().setColor('00FF00');
+                cmd.name ? embed.setTitle(`Help for **\`${cmd.name}\`**`) : null;
+                cmd.aliases.length > 0 ? embed.addField('Aliases', `${cmd.aliases.join(', ')}`) : null;
+                cmd.description ? embed.addField('Description', `${cmd.description}`) : null;
+                cmd.usage ? embed.addField('Usage', `${prefix}${cmd.name} ${cmd.usage}`) : null;
+                cmd.cooldown ? embed.addField('Cooldown', `${cmd.cooldown} seconds`) : null;
 
-				message.channel.send(embed);
-				return;
-			}
-			else {
-				return message.reply('That command doesn\'t exist!');
-			}
+                message.channel.send(embed);
+                return;
+            }
+            else {
+                return message.reply('That command doesn\'t exist!');
+            }
         }
-     }
+    }
 };
