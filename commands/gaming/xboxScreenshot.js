bad93d6eb57b0396f8b2a1f7de0898ded03d0919
LucienHH <66429271+LucienHH@users.noreply.github.com>
2020-10-11 01:37:23 +0100

    Added list function to Gameclips and Screenshots

--- commands/gaming/xboxScreenshot.js
+++ commands/gaming/xboxScreenshot.js
@@ -21,7 +21,7 @@ module.exports = {
                     message.channel.send(embed).then(async msg => {
                         let Gamertag = args[0];
                         try {
-                            if (!isNaN(args[0]) || !args[0] || args[0] === 'recent' || args[0] === 'search' || args[0] === 'oldest') {
+                            if (!isNaN(args[0]) || !args[0] || args[0] === 'recent' || args[0] === 'search' || args[0] === 'oldest' || args[0] === 'list') {
 
                                 Gamertag = result_gamertag && result_gamertag.length == 1 ? result_gamertag[0].gamertag : undefined;
                             }
@@ -132,6 +132,75 @@ module.exports = {
                                     errMsg = `Pick between 1 and ${total}.`;
                                     helpers.embedErr(msg, errMsg);
                                     return;
+                                }
+                                if (args[0] === 'list') {
+                                    const xb = xb2.data.data;
+                                    const maxPage = Math.ceil(total / 10);
+                                    let viewsTotal = 0;
+                                    if (args[1] === '1' || !args[1]) {
+                                        let order = 1;
+                                        let lstring = '';
+                                        for (let i = 0; i < total; i++) {
+                                            if (i > total - 1) {
+                                                break;
+                                            }
+                                            viewsTotal = viewsTotal + xb[i].metadata.views;
+                                            lstring = lstring + `**${order}.** [${xb[i].game.name}](${xb[i].download_urls.source}) | Uploaded ${xb[i].uploaded_at.replace(/T/g, ' ').replace(/Z/g, '').slice(0, 10)}\n`;
+                                            order++;
+                                            if (order > 10) {
+                                                break;
+                                            }
+                                        }
+                                        const embed = new Discord.MessageEmbed()
+                                            .setAuthor(`${xb[0].author.gamertag}`, `${xb[0].author.gamerpic}`)
+                                            .setColor(xb1.data.colors.primary)
+                                            .setTitle('Xbox Screenshots: (page 1)')
+                                            .setDescription(lstring + '')
+                                            .setFooter(`Total Views: ${viewsTotal} | Page 1/${maxPage}`);
+                                        message.channel.send({ embed });
+                                        msg.delete();
+                                        return;
+                                    }
+                                    else {
+                                        const decimal = (args[1] - Math.floor(args[1])) !== 0;
+                                        if (isNaN(args[1])) {
+                                            errMsg = 'You need to specify a page number';
+                                            helpers.embedErr(msg, errMsg);
+                                            return;
+                                        }
+                                        else if (decimal) {
+                                            errMsg = 'That number includes a decimal';
+                                            helpers.embedErr(msg, errMsg);
+                                            return;
+                                        }
+                                        else if (args[1] > maxPage || args[1] < 1) {
+                                            errMsg = `That page doesn't exist. Choose between 1 and ${maxPage}`;
+                                            helpers.embedErr(msg, errMsg);
+                                            return;
+                                        }
+                                        let order = `${args[1] - 1}1`;
+                                        let lstring = '';
+                                        for (let i = (args[1] - 1) * 10; i < total; i++) {
+                                            if (i > total - 1) {
+                                                break;
+                                            }
+                                            viewsTotal = viewsTotal + xb[i].metadata.views;
+                                            lstring = lstring + `**${order}.** [${xb[i].game.name}](${xb[i].download_urls.source}) | Uploaded ${xb[i].uploaded_at.replace(/T/g, ' ').replace(/Z/g, '').slice(0, 10)}\n`;
+                                            order++;
+                                            if (order > `${args[1]}0`) {
+                                                break;
+                                            }
+                                        }
+                                        const embed = new Discord.MessageEmbed()
+                                            .setAuthor(`${xb[0].author.gamertag}`, `${xb[0].author.gamerpic}`)
+                                            .setColor(xb1.data.colors.primary)
+                                            .setTitle(`Xbox Screenshots: (page ${args[1]})`)
+                                            .setDescription(lstring + '')
+                                            .setFooter(`Total Views: ${viewsTotal} | Page ${args[1]}/${maxPage}`);
+                                        message.channel.send({ embed });
+                                        msg.delete();
+                                        return;
+                                    }
                                 }
                                 const xbox = xb2.data.data[num - 1];
                                 const embed = new Discord.MessageEmbed()
