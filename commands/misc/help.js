7734d0e858c39333271b846a5490ef233a257f8e
LucienHH <66429271+LucienHH@users.noreply.github.com>
2020-10-21 23:18:45 +0100

    Help toLowerCase

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -95,7 +95,7 @@ module.exports = {
             message.channel.send(embed)
         }
         else {
-            const command = args[0];
+            const command = args[0].toLowerCase();
             if (client.commands.has(command)) {
                 cmd = client.commands.get(command);
                 const embed = new Discord.MessageEmbed().setColor('00FF00');
