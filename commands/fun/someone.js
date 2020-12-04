adcbb40e73bde8690d13d141ff471eb94296af2e
PatrossDev <contactpatross@gmail.com>
2020-12-04 00:45:14 +0000

    we're back in business@

--- commands/fun/someone.js
+++ commands/fun/someone.js
@@ -8,45 +8,42 @@ module.exports = {
     aliases: ['somebody'],
     description: 'Ping a random person! To get blacklisted use `!someone blacklist`, to whitelist yourself again do `!someone whitelist`. Formerly the main feature of KappaBot. [*]',
     // cooldown: 7200,
-    cooldown: 10,
+    cooldown: 1,
     usage: `!someone`,
     execute(message, args) {
-        return message.channel.send(new Discord.MessageEmbed()
-        .setTitle("This command is temporarily unavailable.")
-        .setColor('#ff0000')
-        .setDescription("Give us time to receive additional intents from discord so that we can bring this command back up.")
-        .setFooter("<3 - Patross"));
-        // //grab a non bot user
-        // message.guild.members.fetch().then(member => {
-        //     const randomUser = member.random();
-        //     helpers.pool.getConnection(async function (err, connection) {
-        //         connection.query(`SELECT * FROM ${process.env.mysql_users_table} WHERE user_id = ${randomUser.id}`, function (err, user_result) {
-        //             if (user_result == undefined) {
-        //                 connection.query(`INSERT INTO ${process.env.mysql_users_table} VALUES (NULL, ${randomUser.id})`)
-        //             } else if (user_result == 0) {
-        //                 connection.query(`INSERT INTO ${process.env.mysql_users_table} VALUES (NULL, ${randomUser.id})`)
-        //             } else {
-        //                 //nothing
-        //             }
-        //         })
+        //grab a non bot user
+        message.guild.members.fetch({ force: true }).then(member => {
+            const filteredUser = member.filter(f => f.user.bot === false);
+            const randomUser = filteredUser.random();
+
+            helpers.pool.getConnection(async function (err, connection) {
+                connection.query(`SELECT * FROM ${process.env.mysql_users_table} WHERE user_id = ${randomUser.id}`, function (err, user_result) {
+                    if (user_result == undefined) {
+                        connection.query(`INSERT INTO ${process.env.mysql_users_table} VALUES (NULL, ${randomUser.id})`)
+                    } else if (user_result == 0) {
+                        connection.query(`INSERT INTO ${process.env.mysql_users_table} VALUES (NULL, ${randomUser.id})`)
+                    } else {
+                        //nothing
+                    }
+                })
 
 
-        //         connection.query(`SELECT * FROM ${process.env.mysql_users_table} WHERE user_id = ${randomUser.id}`, function (err, result) {
-        //             // console.log(result);
-        //             connection.query(`SELECT * FROM ${process.env.mysql_someone_blacklist_table} where user_id = ${result[0].id}`, function (err, results_) {
-        //                 if (results_ && results_.length == 1) {
-        //                     // message.channel.send(`Blacklisted user: ${randomUser.user.username}#${randomUser.user.discriminator}`);
-        //                 } else {
-        //                     // message.channel.send(`<@${randomUser.id}>`);
-        //                     console.log(randomUser.user.username);
-        //                 }
-        //             })
-        //         })
+                connection.query(`SELECT * FROM ${process.env.mysql_users_table} WHERE user_id = ${randomUser.id}`, function (err, result) {
+                    // console.log(result);
+                    connection.query(`SELECT * FROM ${process.env.mysql_someone_blacklist_table} where user_id = ${result[0].id}`, function (err, results_) {
+                        if (results_ && results_.length == 1) {
+                            message.channel.send(`Blacklisted user: ${randomUser.user.username}#${randomUser.user.discriminator}`);
+                        } else {
+                            message.channel.send(`<@${randomUser.id}>`);
+                            console.log(randomUser.user.username);
+                        }
+                    })
+                })
 
 
-        //         connection.release();
-        //     })
-        // });
+                connection.release();
+            })
+        });
 
     }
 };
